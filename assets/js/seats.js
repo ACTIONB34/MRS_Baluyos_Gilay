98c9a7886c4c13b822018cfc5d5b094e5fa5f14e
Chanel <30874903+chanelbaluyos@users.noreply.github.com>
2020-05-04 01:10:03 +0800

    format scripts

--- assets/js/seats.js
+++ assets/js/seats.js
@@ -1,106 +1,106 @@
 $(document).ready(function () {
-  let index = getUrlVars().index;
-  let movie = getMovie(index);
-  let reservation = new Reservation(movie, getSeats(), []);
+	let index = getUrlVars().index;
+	let movie = getMovie(index);
+	let reservation = new Reservation(movie, getSeats(), []);
 
-  updateTicketInfo("#ticketinfo", 0, 0, 0);
-  $("#seatsBlock").append(
-    populateViewSeats(reservation.getSeats(), movie.title)
-  );
+	updateTicketInfo("#ticketinfo", 0, 0, 0);
+	$("#seatsBlock").append(
+		populateViewSeats(reservation.getSeats(), movie.title)
+	);
 
-  $("#checkoutBtn").click(function (e) {
-    e.preventDefault();
-    if (reservation.getSelectedSeats().length > 0) {
-      _addReservation({
-        ticketInfo: {
-          tickets: reservation.getSelectedSeats().length,
-          seats: reservation.getSelectedSeats().join(","),
-          total:
-            reservation.getSelectedSeats().length * reservation.movie.price,
-        },
-        ...movie,
-      });
-      window.location.href =
-        "checkout.html?index=" + (getReservations().length - 1);
-    }
-  });
+	$("#checkoutBtn").click(function (e) {
+		e.preventDefault();
+		if (reservation.getSelectedSeats().length > 0) {
+			_addReservation({
+				ticketInfo: {
+					tickets: reservation.getSelectedSeats().length,
+					seats: reservation.getSelectedSeats().join(","),
+					total:
+						reservation.getSelectedSeats().length * reservation.movie.price,
+				},
+				...movie,
+			});
+			window.location.href =
+				"checkout.html?index=" + (getReservations().length - 1);
+		}
+	});
 
-  $('input[type="checkbox"]').click(function () {
-    let seat = $(this).prop("value");
-    let availablity = $(this).prop("checked");
-    reservation.setSeatAvailability(
-      seat.charAt(0),
-      seat.charAt(1),
-      !availablity
-    );
-    updateTicketInfo(
-      "#ticketinfo",
-      reservation.getSelectedSeats().length,
-      reservation.getSelectedSeats().join(","),
-      reservation.getSelectedSeats().length * reservation.movie.price
-    );
-  });
+	$('input[type="checkbox"]').click(function () {
+		let seat = $(this).prop("value");
+		let availablity = $(this).prop("checked");
+		reservation.setSeatAvailability(
+			seat.charAt(0),
+			seat.charAt(1),
+			!availablity
+		);
+		updateTicketInfo(
+			"#ticketinfo",
+			reservation.getSelectedSeats().length,
+			reservation.getSelectedSeats().join(","),
+			reservation.getSelectedSeats().length * reservation.movie.price
+		);
+	});
 });
 
 function isSeatTaken(seat, title) {
-  let reservations = getReservations();
-  reservations = reservations.filter((item) => item.title === title);
-  if (reservations.length > 0) {
-    reservations = reservations.filter(
-      (item) => item.ticketInfo.seats.indexOf(seat) > 0
-    );
+	let reservations = getReservations();
+	reservations = reservations.filter((item) => item.title === title);
+	if (reservations.length > 0) {
+		reservations = reservations.filter(
+			(item) => item.ticketInfo.seats.indexOf(seat) > 0
+		);
 
-    return reservations.length > 0 ? true : false;
-  }
+		return reservations.length > 0 ? true : false;
+	}
 
-  return false;
+	return false;
 }
 
 function populateViewSeats(seats, movieTitle) {
-  let table = "";
-  Object.keys(seats).map((col, index) => {
-    table += "<tr>";
-    table += '<td class="seats">' + col + "</td>";
-    Object.keys(seats[col]).map((row, index) => {
-      table += "<td>";
-      table +=
-        '<input type="checkbox" class="seats" value="' +
-        (col + row) +
-        '" ' +
-        (isSeatTaken(col + row, movieTitle) ? "disabled" : "") +
-        ">";
-      table += "</td>";
-      if ((index + 1) % 2 == 0) {
-        table += "<td></td>";
-      }
-    });
-    table += "</tr>";
-    if ((index + 1) % 4 == 0) {
-      table += '<tr class="seatVGap"></tr>';
-    }
-  });
-  table +=
-    "<tr><td></td><td><div class='d-flex p-2 bd-highlight bg-light text-dark justify-content-center align-text-top' style='height: 45px'>Door</div></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td><div class='d-flex p-2 bd-highlight bg-light text-dark justify-content-center align-text-top' style='height: 45px'>Door</div></td></tr>";
-  return table;
+	let table = "";
+	Object.keys(seats).map((col, index) => {
+		table += "<tr>";
+		table += '<td class="seats">' + col + "</td>";
+		Object.keys(seats[col]).map((row, index) => {
+			table += "<td>";
+			table +=
+				'<input type="checkbox" class="seats" value="' +
+				(col + row) +
+				'" ' +
+				(isSeatTaken(col + row, movieTitle) ? "disabled" : "") +
+				">";
+			table += "</td>";
+			if ((index + 1) % 2 == 0) {
+				table += "<td></td>";
+			}
+		});
+		table += "</tr>";
+		if ((index + 1) % 4 == 0) {
+			table += '<tr class="seatVGap"></tr>';
+		}
+	});
+	table +=
+		"<tr><td></td><td><div class='d-flex p-2 bd-highlight bg-light text-dark justify-content-center align-text-top' style='height: 45px'>Door</div></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td><div class='d-flex p-2 bd-highlight bg-light text-dark justify-content-center align-text-top' style='height: 45px'>Door</div></td></tr>";
+	return table;
 }
 
 function updateTicketInfo(
-  selector = "#ticketinfo",
-  noOfseats = 0,
-  seats = 0,
-  total = 0
+	selector = "#ticketinfo",
+	noOfseats = 0,
+	seats = 0,
+	total = 0
 ) {
-  $(selector).html(
-    "<h4>Ticket Information</h4><hr>" +
-      "<strong>Tickets: </strong> " +
-      noOfseats +
-      "<br>" +
-      "<strong>Seats selected: </strong>" +
-      seats +
-      "</p><hr>" +
-      "<strong>Total:</strong> P " +
-      total +
-      "<br> <br>" +
-      '<button  class="confirm-btn block" data-toggle="modal" data-target="#confirm_modal">Proceed</button>'
-  );
+	$(selector).html(
+		"<h4>Ticket Information</h4><hr>" +
+			"<strong>Tickets: </strong> " +
+			noOfseats +
+			"<br>" +
+			"<strong>Seats selected: </strong>" +
+			seats +
+			"</p><hr>" +
+			"<strong>Total:</strong> P " +
+			total +
+			"<br> <br>" +
+			'<button  class="confirm-btn block" data-toggle="modal" data-target="#confirm_modal">Proceed</button>'
+	);
 }
--- assets/js/storage.js
+++ assets/js/storage.js
@@ -2,65 +2,66 @@ const cols = ["A", "B", "C", "D", "F", "G", "H", "I"];
 const rows = Array.from(Array(8), (e, i) => i + 1);
 
 let populateSeats = (cols, rows) => {
-  let seats = {};
-  cols.map((col) => {
-    seats[col] = {};
-    rows.map((row) => {
-      seats[col][row] = { isAvailable: true };
-    });
-  });
+	let seats = {};
+	cols.map((col) => {
+		seats[col] = {};
+
+		rows.map((row) => {
+			seats[col][row] = { isAvailable: true };
+		});
+	});
 
   return seats;
 };
 
 function initSeat() {
-  if (!localStorage.getItem("seats")) setSeats(populateSeats(cols, rows));
+	if (!localStorage.getItem("seats")) setSeats(populateSeats(cols, rows));
 }
 
 function resetSeat() {
-  setSeats(populateSeats(cols, rows));
+	setSeats(populateSeats(cols, rows));
 }
 
 function setSeats(seats) {
-  localStorage.setItem("seats", JSON.stringify(seats));
+	localStorage.setItem("seats", JSON.stringify(seats));
 }
 
 function getSeats() {
-  initSeat();
-  return JSON.parse(localStorage.getItem("seats"));
+	initSeat();
+	return JSON.parse(localStorage.getItem("seats"));
 }
 
 function setMovies(movies) {
-  localStorage.setItem("movies", JSON.stringify(movies));
+	localStorage.setItem("movies", JSON.stringify(movies));
 }
 
 function getMovies() {
-  return localStorage.getItem("movies");
+	return localStorage.getItem("movies");
 }
 
 function getMovie(index) {
-  return JSON.parse(localStorage.getItem("movies"))[index];
+	return JSON.parse(localStorage.getItem("movies"))[index];
 }
 
 function setReservations(reservation) {
-  localStorage.setItem("reservations", JSON.stringify(reservation));
+	localStorage.setItem("reservations", JSON.stringify(reservation));
 }
 
 function _addReservation(reservation) {
-  if (!localStorage.getItem("reservations")) setReservations([]);
-  let reservations = JSON.parse(localStorage.getItem("reservations"));
-  reservations.push(reservation);
-  setReservations(reservations);
+	if (!localStorage.getItem("reservations")) setReservations([]);
+		let reservations = JSON.parse(localStorage.getItem("reservations"));
+		reservations.push(reservation);
+		setReservations(reservations);
 }
 
 function getReservations() {
-  if (!localStorage.getItem("reservations")) setReservations([]);
-  return JSON.parse(localStorage.getItem("reservations"));
+	if (!localStorage.getItem("reservations")) setReservations([]);
+	return JSON.parse(localStorage.getItem("reservations"));
 }
 
 function removeReservations(ix, url) {
-  setReservations(getReservations().filter((_, i) => i != ix));
-  if (url) {
-    window.location.href = url;
-  }
+	setReservations(getReservations().filter((_, i) => i != ix));
+	if (url) {
+		window.location.href = url;
+	}
 }
