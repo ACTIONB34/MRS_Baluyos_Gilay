ebed4ce4588ecc32bba20ee891395ed90ccc8fbf
Chanel <chanelbaluyos@gmail.com>
2020-05-03 22:22:23 +0800

    asd

--- assets/js/seats.js
+++ assets/js/seats.js
@@ -1,4 +1,62 @@
-function populateViewSeats(seats) {
+$(document).ready(function () {
+  let index = getUrlVars().index;
+  let movie = getMovie(index);
+  let reservation = new Reservation(movie, getSeats(), []);
+
+  updateTicketInfo("#ticketinfo", 0, 0, 0);
+  $("#seatsBlock").append(
+    populateViewSeats(reservation.getSeats(), movie.title)
+  );
+
+  $("#checkoutBtn").click(function (e) {
+    e.preventDefault();
+    if (reservation.getSelectedSeats().length > 0) {
+      _addReservation({
+        ticketInfo: {
+          tickets: reservation.getSelectedSeats().length,
+          seats: reservation.getSelectedSeats().join(","),
+          total:
+            reservation.getSelectedSeats().length * reservation.movie.price,
+        },
+        ...movie,
+      });
+      window.location.href =
+        "checkout.html?index=" + getReservations().length - 1;
+    }
+  });
+
+  $('input[type="checkbox"]').click(function () {
+    let seat = $(this).prop("value");
+    let availablity = $(this).prop("checked");
+    reservation.setSeatAvailability(
+      seat.charAt(0),
+      seat.charAt(1),
+      !availablity
+    );
+    updateTicketInfo(
+      "#ticketinfo",
+      reservation.getSelectedSeats().length,
+      reservation.getSelectedSeats().join(","),
+      reservation.getSelectedSeats().length * reservation.movie.price
+    );
+  });
+});
+
+function isSeatTaken(seat, title) {
+  let reservations = getReservations();
+  reservations = reservations.filter((item) => item.title === title);
+  if (reservations.length > 0) {
+    reservations = reservations.filter(
+      (item) => item.ticketInfo.seats.indexOf(seat) > 0
+    );
+
+    return reservations.length > 0 ? true : false;
+  }
+
+  return false;
+}
+
+function populateViewSeats(seats, movieTitle) {
   let table = "";
   Object.keys(seats).map((col, index) => {
     table += "<tr>";
@@ -9,7 +67,7 @@ function populateViewSeats(seats) {
         '<input type="checkbox" class="seats" value="' +
         (col + row) +
         '" ' +
-        (!seats[col][row].isAvailable ? "checked" : "") +
+        (isSeatTaken(col + row, movieTitle) ? "disabled" : "") +
         ">";
       table += "</td>";
       if ((index + 1) % 2 == 0) {
@@ -46,44 +104,3 @@ function updateTicketInfo(
       '<button  class="confirm-btn block" data-toggle="modal" data-target="#confirm_modal">Proceed</button>'
   );
 }
-
-$(document).ready(function () {
-  let index = getUrlVars().index;
-  let movie = getMovie(index);
-  let reservation = new Reservation(movie, getSeats(), []);
-
-  updateTicketInfo("#ticketinfo", 0, 0, 0);
-  $("#seatsBlock").append(populateViewSeats(reservation.getSeats()));
-
-  $("#checkoutBtn").click(function (e) {
-    e.preventDefault();
-    if (reservation.getSelectedSeats().length > 0) {
-      _addReservation({
-        ticketInfo: {
-          tickets: reservation.getSelectedSeats().length,
-          seats: reservation.getSelectedSeats().join(","),
-          total:
-            reservation.getSelectedSeats().length * reservation.movie.price,
-        },
-        ...movie,
-      });
-      window.location.href = "checkout.html?index=" + getReservations().length;
-    }
-  });
-
-  $('input[type="checkbox"]').click(function () {
-    let seat = $(this).prop("value");
-    let availablity = $(this).prop("checked");
-    reservation.setSeatAvailability(
-      seat.charAt(0),
-      seat.charAt(1),
-      !availablity
-    );
-    updateTicketInfo(
-      "#ticketinfo",
-      reservation.getSelectedSeats().length,
-      reservation.getSelectedSeats().join(","),
-      reservation.getSelectedSeats().length * reservation.movie.price
-    );
-  });
-});
