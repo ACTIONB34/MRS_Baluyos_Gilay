1849b72f8a28c80cd3288cb11356a08b5a0291df
Chanel <30874903+chanelbaluyos@users.noreply.github.com>
2020-05-05 12:07:05 +0800

    edited buttons and format scripts
    edited select button
    centered close button

--- assets/js/reservations.js
+++ assets/js/reservations.js
@@ -1,67 +1,62 @@
 function populateReservationTable(
-  selector,
-  ticketNo,
-  DateBooked,
-  Movie,
-  Cinema,
-  Time,
-  noOfTicket,
-  seats,
-  Status
+	selector,
+	ticketNo,
+	DateBooked,
+	Movie,
+	Cinema,
+	Time,
+	noOfTicket,
+	seats,
+	Status
 ) {
-  let content =
-    "<tr>" +
-    "<td>" +
-    '<div class="social-links text-center text-md-right pt-3 pt-md-0">' +
-    '<a href="checkout.html?index=' +
-    ticketNo +
-    '" class="linkedin"><i class="icofont-look"></i></a>' +
-    '<a href="#" onClick="removeReservations(\'' +
-    ticketNo +
-    '\',\'reservations.html\')" class="linkedin"><i class="icofont-ui-delete"></i></a>' +
-    "</div>" +
-    "</td>" +
-    "<td>" +
-    ticketNo +
-    "</td>" +
-    "<td>" +
-    DateBooked +
-    "</td>" +
-    "<td>" +
-    Movie +
-    "</td>" +
-    "<td>" +
-    Cinema +
-    "</td>" +
-    "<td>" +
-    Time +
-    "</td>" +
-    "<td>" +
-    noOfTicket +
-    "</td>" +
-    "<td>" +
-    seats +
-    "</td>" +
-    "<td>Pending</td>" +
-    "</tr>";
+	let content =
+		"<tr>" +
+			"<td>" +
+				'<div class="social-links text-center text-md-right pt-3 pt-md-0">' +
+					'<a href="checkout.html?index=' + ticketNo + '" class="linkedin"><i class="icofont-look"></i></a>' +
+					'<a href="#" onClick="removeReservations(\'' + ticketNo + '\',\'reservations.html\')" class="linkedin"><i class="icofont-ui-delete"></i></a>' +
+				"</div>" +
+			"</td>" +
+			"<td>" + ticketNo +
+			"</td>" +
+			"<td>" + DateBooked +
+			"</td>" +
+			"<td>" + Movie +
+			"</td>" +
+			"<td>" + Cinema +
+			"</td>" +
+			"<td>" + Time +
+			"</td>" +
+			"<td>" + noOfTicket +
+			"</td>" +
+			"<td>" + seats +
+			"</td>" +
+			"<td>Pending</td>" +
+		"</tr>";
 
-  $(selector).children("tbody").append(content);
+	$(selector).children("tbody").append(content);
 }
 
-$(function () {
-  let reservations = getReservations();
-  reservations.map(
-    ({ index, showDate, title, scheduleInfo, ticketInfo }, rindex) => {
-      populateReservationTable(
-        "#reservationTable",
-        rindex,
-        scheduleInfo.date,
-        title,
-        scheduleInfo.cinema,
-        scheduleInfo.timeRange,
-        ticketInfo.tickets,
-        ticketInfo.seats
-      );
-    }
-  );
+$(function() {
+	let reservations = getReservations();
+	reservations.map(
+		({
+			index,
+			showDate,
+			title,
+			scheduleInfo,
+			ticketInfo
+		}, rindex) => {
+			populateReservationTable(
+				"#reservationTable",
+				rindex,
+				scheduleInfo.date,
+				title,
+				scheduleInfo.cinema,
+				scheduleInfo.timeRange,
+				ticketInfo.tickets,
+				ticketInfo.seats
+			);
+		}
+	);
 });
--- assets/js/seats.js
+++ assets/js/seats.js
@@ -1,117 +1,125 @@
-$(document).ready(function () {
-  let index = getUrlVars().index;
-  let schedule = getScheduleByID(index);
-  let movie = getMovie(schedule.movieID);
-  let reservation = new Reservation(movie, getSeats(), schedule, []);
+$(document).ready(function() {
+	let index = getUrlVars().index;
+	let schedule = getScheduleByID(index);
+	let movie = getMovie(schedule.movieID);
+	let reservation = new Reservation(movie, getSeats(), schedule, []);
 
-  updateTicketInfo("#ticketinfo", 0, 0, 0);
+	updateTicketInfo("#ticketinfo", 0, 0, 0);
 
-  $("#seatsBlock").append(
-    populateViewSeats(reservation.getSeats(), schedule.id)
-  );
+	$("#seatsBlock").append(
+		populateViewSeats(reservation.getSeats(), schedule.id)
+	);
 
-  $("#checkoutBtn").click(function (e) {
-    e.preventDefault();
-    if (reservation.getSelectedSeats().length > 0) {
-      _addReservation({
-        scheduleInfo: { ...reservation.schedule },
-        ticketInfo: {
-          tickets: reservation.getSelectedSeats().length,
-          seats: reservation.getSelectedSeats().join(", "),
-          total:
-            reservation.getSelectedSeats().length * reservation.schedule.price,
-        },
-        ...movie,
-      });
-      window.location.href =
-        "checkout.html?index=" + (getReservations().length - 1);
-    } else {
-      $("#confirm_modal .modal-body").text("You have not selected any seats.");
-      $("#confirm_modal .modal-footer").hide();
-    }
-  });
+	$("#checkoutBtn").click(function(e) {
+		e.preventDefault();
+		if (reservation.getSelectedSeats().length > 0) {
+			_addReservation({
+				scheduleInfo: {
+					...reservation.schedule
+				},
+				ticketInfo: {
+					tickets: reservation.getSelectedSeats().length,
+					seats: reservation.getSelectedSeats().join(", "),
+					total: reservation.getSelectedSeats().length * reservation.schedule.price,
+				},
+				...movie,
+			});
+			window.location.href =
+				"checkout.html?index=" + (getReservations().length - 1);
+		} else {
+			$("#confirm_modal .modal-body").text("You have not selected any seats.");
+			$("#confirm_modal .modal-footer").hide();
+		}
+	});
 
-  $('input[type="checkbox"]').click(function () {
-    let seat = $(this).prop("value");
-    let availablity = $(this).prop("checked");
-    reservation.setSeatAvailability(
-      seat.charAt(0),
-      seat.charAt(1),
-      !availablity
-    );
+	$('input[type="checkbox"]').click(function() {
+		let seat = $(this).prop("value");
+		let availablity = $(this).prop("checked");
+		reservation.setSeatAvailability(
+			seat.charAt(0),
+			seat.charAt(1),
+			!availablity
+		);
 
-    updateTicketInfo(
-      "#ticketinfo",
-      reservation.getSelectedSeats().length,
-      reservation.getSelectedSeats().join(", "),
-      reservation.getSelectedSeats().length * reservation.schedule.price
-    );
-  });
+		updateTicketInfo(
+			"#ticketinfo",
+			reservation.getSelectedSeats().length,
+			reservation.getSelectedSeats().join(", "),
+			reservation.getSelectedSeats().length * reservation.schedule.price
+		);
+	});
 });
 
 function isSeatTaken(seat, scheduleId) {
-  let reservations = getReservations();
-  reservations = reservations.filter(
-    (item) => item.scheduleInfo.id == scheduleId
-  );
-  if (reservations.length > 0) {
-    reservations = reservations.filter(
-      (item) => item.ticketInfo.seats.indexOf(seat) >= 0
-    );
+	let reservations = getReservations();
+	reservations = reservations.filter(
+		(item) => item.scheduleInfo.id == scheduleId
+	);
+	if (reservations.length > 0) {
+		reservations = reservations.filter(
+			(item) => item.ticketInfo.seats.indexOf(seat) >= 0
+		);
 
-    return reservations.length > 0 ? true : false;
-  }
+		return reservations.length > 0 ? true : false;
+	}
 
-  return false;
+	return false;
 }
 
 function populateViewSeats(seats, scheduleId) {
-  let table = "";
-  Object.keys(seats).map((col, index) => {
-    table += "<tr>";
-    table += '<td class="seats">' + col + "</td>";
-    Object.keys(seats[col]).map((row, index) => {
-      table += "<td>";
-      table +=
-        '<input type="checkbox" class="seats" value="' +
-        (col + row) +
-        '" ' +
-        (isSeatTaken(col + row, scheduleId) ? "disabled" : "") +
-        ">";
-      table += "</td>";
-      if ((index + 1) % 2 == 0) {
-        table += "<td></td>";
-      }
-    });
-    table += "</tr>";
-    if ((index + 1) % 3 == 0) {
-      table += '<tr class="seatVGap"></tr>';
-    }
-  });
-  table +=
-    "<tr><td></td><td><div class='d-flex p-2 bd-highlight bg-light text-dark justify-content-center align-text-top' style='height: 45px'>Door</div></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td><div class='d-flex p-2 bd-highlight bg-light text-dark justify-content-center align-text-top' style='height: 45px'>Door</div></td></tr>";
-  return table;
+	let table = "";
+	Object.keys(seats).map((col, index) => {
+		table += "<tr>";
+		table += '<td class="seats">' + col + "</td>";
+		Object.keys(seats[col]).map((row, index) => {
+			table += "<td>";
+			table +=
+				'<input type="checkbox" class="seats" value="' +
+				(col + row) +
+				'" ' +
+				(isSeatTaken(col + row, scheduleId) ? "disabled" : "") +
+				">";
+			table += "</td>";
+			if ((index + 1) % 2 == 0) {
+				table += "<td></td>";
+			}
+		});
+		table += "</tr>";
+		if ((index + 1) % 3 == 0) {
+			table += '<tr class="seatVGap"></tr>';
+		}
+	});
+	table +=
+		"<tr>" +
+			"<td></td>" +
+			"<td><div class='d-flex p-2 bd-highlight bg-light text-dark justify-content-center align-text-top' style='height: 45px'>Door</div></td>" +
+			"<td></td>" +
+			"<td></td>" +
+			"<td></td>" +
+			"<td></td>" +
+			"<td></td>" +
+			"<td></td>" +
+			"<td></td>" +
+			"<td></td>" +
+			"<td></td>" +
+			"<td><div class='d-flex p-2 bd-highlight bg-light text-dark justify-content-center align-text-top' style='height: 45px'>Door</div></td>" +
+		"</tr>";
+	return table;
 }
 
 function updateTicketInfo(
-  selector = "#ticketinfo",
-  noOfseats = 0,
-  seats = 0,
-  total = 0
+	selector = "#ticketinfo",
+	noOfseats = 0,
+	seats = 0,
+	total = 0
 ) {
-  $(selector).html(
-    "<h4>Ticket Information</h4><hr>" +
-      "<strong>Tickets: </strong> " +
-      noOfseats +
-      "<br>" +
-      "<strong>Seats selected: </strong>" +
-      seats +
-      "</p><hr>" +
-      "<strong>Total:</strong> P " +
-      total +
-      "<br> <br>" +
-      (noOfseats > 0
-        ? '<button  class="confirm-btn block" data-toggle="modal" data-target="#confirm_modal" >Proceed</button>'
-        : "")
-  );
+	$(selector).html(
+		"<h4>Ticket Information</h4><hr>" +
+		"<strong>Tickets: </strong> " + noOfseats + "<br>" +
+		"<strong>Seats selected: </strong>" + seats + "</p><hr>" +
+		"<strong>Total:</strong> P " + 	total + "<br> <br>" +
+		(noOfseats > 0 ?
+			'<button  class="confirm-btn block" data-toggle="modal" data-target="#confirm_modal" >Proceed</button>' :
+			"")
+	);
 }
